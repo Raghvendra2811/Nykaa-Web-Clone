f39fa3a1ce9579fc130cd9a397a66fea3ae7546c
Ankit <ranaankitr@outlook.com>
2022-09-05 14:04:50 +0530

    Cart UI

---
+++ cart.js
@@ -0,0 +1,217 @@
+var cartProducts= [
+    {    image_url:
+        "https://content.shop4reebok.com/static/Product-EX4296/reebok_EX4296_1.jpg.plp",
+    name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+    price: "1170",
+    strikedoffprice: "2599",
+    },
+    {
+    image_url:
+        "https://content.shop4reebok.com/static/Product-EX3921/reebok_EX3921_1.jpg.plp",
+    name: "MEN'S REEBOK SWIM ARUBA FLIP SLIPPERS ",
+    price: 699,
+    strikedoffprice: 999,
+    },
+    {
+    image_url:
+        "https://content.shop4reebok.com/static/Product-EX4160/reebok_EX4160_1.jpg.plp",
+    name: " MEN'S REEBOK RUNNING AUSTIN SHOES",
+    price: 945,
+    strikedoffprice: 2099,
+    },
+    {
+    image_url:
+        "https://content.shop4reebok.com/static/Product-FV8798/MEN_SWIM_SLIPPERS_FV8798_1.jpg.plp",
+    name: "MEN'S REEBOK SWIM AVENGER FLIP LP SLIPPERS",
+    price: 399,
+    strikedoffprice: 799,
+    },
+    {
+    image_url:
+        "https://content.shop4reebok.com/static/Product-EW5173/reebok_EW5173_1.jpg.plp",
+    name: "MEN'S REEBOK RUNNING TEMPO SHOES ",
+    price: 1620,
+    strikedoffprice: 3599,
+    },
+    {
+    image_url:
+        "https://content.shop4reebok.com/static/Product-FV7329/reebok_FV7329_1.jpg.plp ",
+    name: " MEN'S REEBOK RUNNING ASTRO BOOSTER SHOES ",
+    price: 1440,
+    strikedoffprice: 3199,
+    },
+    {
+    image_url:
+        "https://content.shop4reebok.com/static/Product-GD8567/MEN_TRAINING_PANTS_GD8567_1.jpg.plp ",
+    name: " MEN'S TRAINING WORKOUT READY ELITAGE PANTS ",
+    price: 1999,
+    strikedoffprice: 3199,
+    },
+    {
+    image_url:
+        "https://content.shop4reebok.com/static/Product-GQ8758/reebok_GQ8758_1.jpg.plp ",
+    name: "MEN'S REEBOK TRAINING  POLO T-SHIRTS TEE  ",
+    price: 769,
+    strikedoffprice: 1099,
+    },
+    {    image_url:
+        "https://content.shop4reebok.com/static/Product-EX4296/reebok_EX4296_1.jpg.plp",
+    name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+    price: "1170",
+    strikedoffprice: "2599",
+    },
+    {
+    image_url:
+        "https://content.shop4reebok.com/static/Product-EX3921/reebok_EX3921_1.jpg.plp",
+    name: "MEN'S REEBOK SWIM ARUBA FLIP SLIPPERS ",
+    price: 699,
+    strikedoffprice: 999,
+    },
+    {
+    image_url:
+        "https://content.shop4reebok.com/static/Product-EX4160/reebok_EX4160_1.jpg.plp",
+    name: " MEN'S REEBOK RUNNING AUSTIN SHOES",
+    price: 945,
+    strikedoffprice: 2099,
+    },
+    {
+    image_url:
+        "https://content.shop4reebok.com/static/Product-FV8798/MEN_SWIM_SLIPPERS_FV8798_1.jpg.plp",
+    name: "MEN'S REEBOK SWIM AVENGER FLIP LP SLIPPERS",
+    price: 399,
+    strikedoffprice: 799,
+    },
+    {
+    image_url:
+        "https://content.shop4reebok.com/static/Product-EW5173/reebok_EW5173_1.jpg.plp",
+    name: "MEN'S REEBOK RUNNING TEMPO SHOES ",
+    price: 1620,
+    strikedoffprice: 3599,
+    },
+    {
+    image_url:
+        "https://content.shop4reebok.com/static/Product-FV7329/reebok_FV7329_1.jpg.plp ",
+    name: " MEN'S REEBOK RUNNING ASTRO BOOSTER SHOES ",
+    price: 1440,
+    strikedoffprice: 3199,
+    },
+    {
+    image_url:
+        "https://content.shop4reebok.com/static/Product-GD8567/MEN_TRAINING_PANTS_GD8567_1.jpg.plp ",
+    name: " MEN'S TRAINING WORKOUT READY ELITAGE PANTS ",
+    price: 1999,
+    strikedoffprice: 3199,
+    },
+    {
+    image_url:
+        "https://content.shop4reebok.com/static/Product-GQ8758/reebok_GQ8758_1.jpg.plp ",
+    name: "MEN'S REEBOK TRAINING  POLO T-SHIRTS TEE  ",
+    price: 769,
+    strikedoffprice: 1099,
+    },{    image_url:
+        "https://content.shop4reebok.com/static/Product-EX4296/reebok_EX4296_1.jpg.plp",
+    name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+    price: "1170",
+    strikedoffprice: "2599",
+    },
+    {
+    image_url:
+        "https://content.shop4reebok.com/static/Product-EX3921/reebok_EX3921_1.jpg.plp",
+    name: "MEN'S REEBOK SWIM ARUBA FLIP SLIPPERS ",
+    price: 699,
+    strikedoffprice: 999,
+    },
+    {
+    image_url:
+        "https://content.shop4reebok.com/static/Product-EX4160/reebok_EX4160_1.jpg.plp",
+    name: " MEN'S REEBOK RUNNING AUSTIN SHOES",
+    price: 945,
+    strikedoffprice: 2099,
+    },
+    {
+    image_url:
+        "https://content.shop4reebok.com/static/Product-FV8798/MEN_SWIM_SLIPPERS_FV8798_1.jpg.plp",
+    name: "MEN'S REEBOK SWIM AVENGER FLIP LP SLIPPERS",
+    price: 399,
+    strikedoffprice: 799,
+    },
+    {
+    image_url:
+        "https://content.shop4reebok.com/static/Product-EW5173/reebok_EW5173_1.jpg.plp",
+    name: "MEN'S REEBOK RUNNING TEMPO SHOES ",
+    price: 1620,
+    strikedoffprice: 3599,
+    },
+    {
+    image_url:
+        "https://content.shop4reebok.com/static/Product-FV7329/reebok_FV7329_1.jpg.plp ",
+    name: " MEN'S REEBOK RUNNING ASTRO BOOSTER SHOES ",
+    price: 1440,
+    strikedoffprice: 3199,
+    },
+    {
+    image_url:
+        "https://content.shop4reebok.com/static/Product-GD8567/MEN_TRAINING_PANTS_GD8567_1.jpg.plp ",
+    name: " MEN'S TRAINING WORKOUT READY ELITAGE PANTS ",
+    price: 1999,
+    strikedoffprice: 3199,
+    },
+    {
+    image_url:
+        "https://content.shop4reebok.com/static/Product-GQ8758/reebok_GQ8758_1.jpg.plp ",
+    name: "MEN'S REEBOK TRAINING  POLO T-SHIRTS TEE  ",
+    price: 769,
+    strikedoffprice: 1099,
+    }
+];
+
+
+display(cartProducts);
+
+function display(products){
+
+    document.querySelector("#products").innerText="";
+    products.map(function (elem){
+        
+        let productDiv = document.createElement("div");
+        let productDescription =document.createElement("div");
+        let prodcutimg = document.createElement("img");
+        let productName= document.createElement("p");
+        let deleteIcon = document.createElement("div");
+        let amountDiv = document.createElement("div");
+        let qtyDiv = document.createElement("div");
+        let priceDiv = document.createElement("div");
+        let discountAmt = document.createElement("p");
+        let actualAmt = document.createElement("h4");
+
+        prodcutimg.setAttribute("src",elem.image_url);
+        productName.innerText=elem.name;
+        deleteIcon.innerHTML ='<i class="fa-solid fa-trash"></i>';
+        qtyDiv.innerHTML='<p>Quantity</p> : <select> <option value="1">1</option> <option value="2">2</option> <option value="3">3</option> <option value="4">4</option> <option value="5">5</option> </select>'
+        discountAmt.innerText = elem.strikedoffprice;
+        actualAmt.innerText =elem.price;
+
+        priceDiv.append(discountAmt,actualAmt);
+        productDescription.append(prodcutimg,productName,deleteIcon);
+        productDescription.setAttribute("id","productDescription");
+        amountDiv.append(qtyDiv,priceDiv);
+        amountDiv.setAttribute("id","amountdiv");
+        productDiv.append(productDescription,amountDiv);
+        document.getElementById("products").append(productDiv);
+        productDiv.setAttribute("id","productDiv");
+        qtyDiv.setAttribute("id","qtyDiv");
+
+        
+
+
+
+
+
+    })
+    
+
+
+
+
+
+}
